845e1a7a4f78cfdeef69e79df1c3daa3f1367a9a
Brian Barrett <bwbarre@sandia.gov>
2009-07-21 22:28:58 +0000

    Move to a more dynamic component system, using libltdl to be a bit more portable.  It is no longer necessary to add component information to configure.ac or components/Makefile, and componetns can provide a little stub configure.m4 macro to determine if they can compile or not.

---
+++ acinclude.m4
@@ -0,0 +1,3 @@
+dnl -*- Autoconf -*-
+
+m4_include(config/sst_m4_config_include.m4)
--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-echo "Generating configure files..."
-
+echo "Finding Autotools versions ..."
 version=`libtool --version 2>&1`
 if test "$?" != 0 ; then
   echo "Could not find libtool version number.  Aborting."
@@ -11,13 +10,56 @@ version=`echo $version | cut -f2 -d')'`
 version=`echo $version | cut -f1 -d' ' | sed -e 's/[A-Za-z]//g'`
 major_version=`echo $version | cut -f1 -d.`
 
+
+# do component magic
+echo "Finding components ..."
+component_list=
+component_m4_list=
+for component_dir in components/* ; do
+  if test -d "$component_dir" ; then
+    component=`basename "$component_dir"`
+    if test -f "$component_dir/.ignore" -a ! -f "$component_dir/.unignore" ; then
+      echo " - ignoring component $component"
+    elif test -f "$component_dir/.ignore" && \
+         test -s .unignore && \
+         test -z "`grep $USER .unignore`" ; then
+      echo " - ignoring component $component"
+    else
+      if test -z "$component_list" ; then
+        component_list="$component"
+      else
+        component_list="$component_list, $component"
+      fi
+      if test -f "$component_dir/configure.m4" ; then
+        component_m4_list="$component_m4_list $component_dir/configure.m4"
+      fi
+    fi
+  fi
+done
+
+cat > "config/sst_m4_config_include.m4" <<EOF
+dnl -*- Autoconf -*-
+
+dnl This file is automatically created by autogen.sh; it should not
+dnl be edited by hand!!
+
+EOF
+
+echo "m4_define([sst_component_list], [$component_list])" >> config/sst_m4_config_include.m4
+
+for component in $component_m4_list ; do
+  echo "m4_include($component)" >> config/sst_m4_config_include.m4
+done
+
+
+echo "Generating configure files ..."
 if test $major_version -lt 2 ; then
-  echo "- Using Libtool pre-2.0"
+  echo " - Using Libtool pre-2.0"
 
   rm -rf libltdl sst/libltdl
   libtoolize --automake --copy --ltdl
   if test -d libltdl; then
-    echo "- Moving libltdl to sst/"
+    echo " - Moving libltdl to sst/"
     mv libltdl sst
   fi
   if test ! -d sst/libltdl ; then
@@ -30,7 +72,7 @@ if test $major_version -lt 2 ; then
   automake --foreign --add-missing --include-deps
 
 else
-  echo "- Using Libtool 2.0 or higher"
+  echo " - Using Libtool 2.0 or higher"
   autoreconf --install --symlink --warnings=gnu,obsolete,override,portability,no-obsolete
   if test $? != 0 ; then
     echo "It appears that configure file generation failed.  Sorry :(."
