d80086a2788af157b158daaf2124c4446771a166
K. Scott Hemmert <kshemme@sandia.gov>
2010-06-17 16:19:07 +0000

    Still working on new event hierarchy.

---
+++ sst/core/action.h
@@ -0,0 +1,43 @@
+// Copyright 2009-2010 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+// 
+// Copyright (c) 2009-2010, Sandia Corporation
+// All rights reserved.
+// 
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#ifndef SST_ACTION_H
+#define SST_ACTION_H
+
+#include <sst/core/sst.h>
+
+namespace SST {
+
+    class Action : public Activity {
+public:
+    Action() {}
+    ~Action() {}
+
+protected:
+
+    void endSimulation() {
+	Simulation::getSimulation()->endSimulation();
+    }
+    
+private:
+    
+    friend class boost::serialization::access;
+    template<class Archive>
+    void
+    serialize(Archive & ar, const unsigned int version )
+    {
+    }
+};
+
+}
+
+#endif // SST_ACTION_H
