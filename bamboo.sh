fe6c0464e5a5e463cd9fe23ee5c8750a2aa2a77a
John Van Dyke <jpvandy@sandia.gov>
2012-11-08 15:57:50 +0000

    Back out one line from r3594 that had unexpected consequences with respect to DRAMSim.   R-3594 added DRAMSim to the LD_LIBRARY_PATH. Even without configuring DRAMSim it had effects.

--- bamboo.sh
+++ bamboo.sh
@@ -446,7 +446,7 @@ dobuild() {
     fi
 
     echo "==================== Building SST ===================="
-    export LD_LIBRARY_PATH=${SST_INSTALL_DEPS}/lib:${SST_INSTALL_DEPS}/lib/sst:${SST_DEPS_INSTALL_GEM5SST}:${SST_INSTALL_DEPS}/packages/DRAMSim:${LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=${SST_INSTALL_DEPS}/lib:${SST_INSTALL_DEPS}/lib/sst:${SST_DEPS_INSTALL_GEM5SST}:${LD_LIBRARY_PATH}
     # Mac OS X needs some help finding dylibs
     if [ $kernel == "Darwin" ]
     then
