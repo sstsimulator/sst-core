9cddacd2e9de530cc3feb13c24496d21fce8cd59
John Van Dyke <jpvandy@sandia.gov>
2013-01-09 15:30:07 +0000

    Check-in and activate the three zesto tests using previously existing XML files and not requiring Qsim.

--- bamboo.sh
+++ bamboo.sh
@@ -159,6 +159,7 @@ echo " Arg in is $1,  kernel is ${kernel} "
     # Add other test suites here, i.e.
     ${SST_TEST_SUITES}/testSuite_scheduler.sh
     ${SST_TEST_SUITES}/testSuite_patterns.sh
+    ${SST_TEST_SUITES}/testSuite_zesto.sh
 
     # ${SST_TEST_SUITES}/testSuite_moe.sh
     # ${SST_TEST_SUITES}/testSuite_larry.sh
