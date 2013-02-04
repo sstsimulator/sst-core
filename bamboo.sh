78c97c810fcfade5da3c9910db6e657d1667aeb6
Jon Wilson <jwilso@sandia.gov>
2013-02-04 23:54:27 +0000

    Don't run Zesto tests on MacOS X

--- bamboo.sh
+++ bamboo.sh
@@ -177,7 +177,6 @@ echo " Arg in is $1,  kernel is ${kernel} "
     # Add other test suites here, i.e.
     ${SST_TEST_SUITES}/testSuite_scheduler.sh
     ${SST_TEST_SUITES}/testSuite_patterns.sh
-    ${SST_TEST_SUITES}/testSuite_zesto.sh
 
     # ${SST_TEST_SUITES}/testSuite_moe.sh
     # ${SST_TEST_SUITES}/testSuite_larry.sh
