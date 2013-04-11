0c1efbe797546d6aba6413f6e4e1c96eb5097771
John Van Dyke <jpvandy@sandia.gov>
2013-04-11 23:44:27 +0000

    Turn on the memHierarchy test in the nightly.   There are14 tests with two failure-to-completes on MacOS, sdl4-2 and sdl-2

--- bamboo.sh
+++ bamboo.sh
@@ -178,6 +178,8 @@ echo " Arg in is $1,  kernel is ${kernel} "
         fi
     fi
     # Add other test suites here, i.e.
+    ${SST_TEST_SUITES}/testSuite_memHierarchy_sdl.sh
+    ${SST_TEST_SUITES}/testSuite_memHierarchy_bin.sh
 ##    ${SST_TEST_SUITES}/testSuite_scheduler.sh
 ##    ${SST_TEST_SUITES}/testSuite_patterns.sh
 
