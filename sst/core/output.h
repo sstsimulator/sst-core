cc972676e4be97168250fbeab568f95ef1550cd9
Branden J. Moore <bjmoor@sandia.gov>
2013-07-25 22:55:24 +0000

    Fix up includes for proper __STDC_FORMAT_MACROS definition. Define __STDC_FORMAT_MACROS by default in configure.

--- sst/core/output.h
+++ sst/core/output.h
@@ -17,11 +17,7 @@
 // CHOOSE THE --enable-debug OPTION DURING SST CONFIGURATION
 //#define __SST_DEBUG_OUTPUT__
 
-#ifndef __STDC_FORMAT_MACROS
-#define __STDC_FORMAT_MACROS
-#endif
 #include <inttypes.h>
-
 #include <cstdio>
 
 namespace SST {
