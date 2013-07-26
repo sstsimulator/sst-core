ab0d14b5705c3c795596f81b527304bb1eba6e14
John Van Dyke <jpvandy@sandia.gov>
2013-07-26 15:09:49 +0000

    Disable build and test of Zesto - See Issue 99

--- bamboo.sh
+++ bamboo.sh
@@ -107,7 +107,8 @@ dotests() {
     then
         # Only run if the OS *isn't* Darwin (MacOS)
         ${SST_TEST_SUITES}/testSuite_macsim.sh
-        ${SST_TEST_SUITES}/testSuite_zesto.sh
+##            Disable second zesto test -- July 26, 2013
+##        ${SST_TEST_SUITES}/testSuite_zesto.sh
 ##        ${SST_TEST_SUITES}/testSuite_sst_mcniagara.sh
         #      zesto with qsim tests to not run correctly anywhere Feb. 4, 2013
         ## ${SST_TEST_SUITES}/testSuite_zesto_qsimlib.sh
