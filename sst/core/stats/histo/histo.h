3892e172860444f754d1da710b913233f824c79d
Si Hammond <simon.hammond@gmail.com>
2014-04-05 03:40:35 +0000

    Changes to histogram to avoid memory bloat - use a map instead as these may grow to be large over time. Also allows bin types to differ from count types (e.g. double bin width with an integer count type).

--- sst/core/stats/histo/histo.h
+++ sst/core/stats/histo/histo.h
@@ -2,6 +2,9 @@
 #ifndef _H_SST_CORE_STATS_HISTO
 #define _H_SST_CORE_STATS_HISTO
 
+#include <sst_config.h>
+#include <sst/core/output.h>
+
 #include <stdint.h>
 #include <map>
 
@@ -10,115 +13,78 @@ using namespace std;
 namespace SST {
 namespace Statistics {
 
-template<class HistoType>
+template<class HistoBinType, class HistoCountType>
 class Histogram {
 	public:
-		Histogram(HistoType binW) {
-			binCount = 0;
-			binWidth = binW;
-			bins = NULL;
+		Histogram(HistoBinType binW) {
 			totalSummed = 0;
 			itemCount = 0;
+			binWidth = binW;
+			minVal = 0;
+			maxVal = 0;
 		}
 
-		void add(HistoType value) {
-			if(NULL == bins) {
-				bins = (HistoType*) malloc(sizeof(HistoType));
-				bins[0] = 1;
-				binCount = 1;
-
-				minVal = (value - (value % binWidth));
-				maxVal = minVal + binWidth;
-
-				itemCount = 1;
-				totalSummed = value;
+		void add(HistoBinType value) {
+			HistoBinType bin_start = value - (value % binWidth);
+			histo_itr bin_itr = bins.find(bin_start);
+			
+			if(bin_itr == bins.end()) {
+				bins.insert(std::pair<HistoBinType, HistoCountType>(bin_start, 1));
 			} else {
-				if(value < minVal) {
-					HistoType newLower = value - (value % binWidth);
-					HistoType diff = minVal - newLower;
-					int diff_bins = diff / binWidth;
-
-					binCount += (uint32_t) diff_bins;
-					HistoType* newBins = (HistoType*) malloc(binCount * sizeof(HistoType));
-
-					for(uint32_t i = 0; i < binCount; ++i) {
-						newBins[i] = 0;
-					}
-
-					for(uint32_t i = (uint32_t) diff_bins; i < binCount; ++i) {
-						newBins[i] = bins[i - diff_bins];
-					}
-
-					free(bins);
-					bins = newBins;
-					minVal = newLower;
-				} else if (value >= maxVal) {
-					HistoType newMax = value + (binWidth - (value % binWidth));
-					HistoType diff = newMax - maxVal;
-					int diff_bins = diff / binWidth;
-
-					binCount += (uint32_t) diff_bins;
-					HistoType* newBins = (HistoType*) malloc(binCount * sizeof(HistoType));
-
-					for(uint32_t i = 0; i < binCount; ++i) {
-						newBins[i] = 0;
-					}
-
-					for(uint32_t i = 0; i < (uint32_t) (binCount - diff_bins); ++i) {
-						newBins[i] = bins[i];
-					}
-
-					free(bins);
-					bins = newBins;
-					maxVal = newMax;
-				}
-
-				HistoType value_from_base = value - minVal;
-				int inc_element = value_from_base / binWidth;
-
-				bins[inc_element]++;
-
-				totalSummed += value;
-				itemCount++;
+				bin_itr->second++;
 			}
+			
+			totalSummed += value;
+			
+			minVal = (minVal < bin_start) ? minVal : bin_start;
+			maxVal = (maxVal > bin_start) ? maxVal : bin_start;
+			
 		}
 
-		uint32_t getBinCount() {
-			return binCount;
+		HistoCountType getBinCount() {
+			return bins.size();
 		}
 
-		HistoType getBinWidth() {
+		HistoBinType getBinWidth() {
 			return binWidth;
 		}
 
-		HistoType* getBinByIndex(int index) {
-			return &bins[index];
+		HistoCountType getBinCountByBinStart(HistoBinType v) {
+			histo_itr bin_itr = bins.find(v);
+			
+			if(bin_itr == bins.end()) {
+				return (HistoCountType) 0;
+			} else {
+				return bins[v];
+			}
 		}
 
-		HistoType getBinStart() {
+		HistoBinType getBinStart() {
 			return minVal;
 		}
 
-		HistoType getBinMax() {
+		HistoBinType getBinEnd() {
 			return maxVal;
 		}
 
-		uint64_t getItemCount() {
+		HistoCountType getItemCount() {
 			return itemCount;
 		}
 
-		HistoType getValuesSummed() {
+		HistoBinType getValuesSummed() {
 			return totalSummed;
 		}
-
+		
+		typedef typename std::map<HistoBinType, HistoCountType>::iterator histo_itr;
+		
 	private:
-		HistoType* bins;
-		HistoType binWidth;
-		HistoType minVal;
-		HistoType maxVal;
-		HistoType totalSummed;
-		uint64_t itemCount;
-		uint32_t binCount;
+		HistoBinType minVal;
+		HistoBinType maxVal;
+		HistoBinType binWidth;
+		HistoBinType totalSummed;
+		HistoCountType itemCount;
+
+		std::map<HistoBinType, HistoCountType> bins;
 };
 
 }
