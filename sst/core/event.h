50941862be887629a4ad714547aeb369e2fcc287
Branden J. Moore <bjmoor@sandia.gov>
2014-04-07 18:50:55 +0000

    Doxygen commented SST::Event

--- sst/core/event.h
+++ sst/core/event.h
@@ -23,85 +23,113 @@ namespace SST {
 
 class Link;
 
+/**
+ * Base class for Events - Items sent across links to communicate between
+ * components.
+ */
 class Event : public Activity {
 public:
     Event() : Activity() {
-	setPriority(50);
+        setPriority(50);
     }
     virtual ~Event() = 0;
 
+    /** Cause this event to fire */
     void execute(void);
 
+    /** Sets the link id used for delivery.  For use by SST Core only */
     inline void setDeliveryLink(LinkId_t id, Link *link) {
-	link_id = id;
-	delivery_link = link;
+        link_id = id;
+        delivery_link = link;
     }
 
+    /** Gets the link id used for delivery.  For use by SST Core only */
     inline Link* getDeliveryLink() {
         return delivery_link;
     }
 
+    /** For use by SST Core only */
     inline void setRemoteEvent() {
-	delivery_link = NULL;
+        delivery_link = NULL;
     }
 
+    /** Gets the link id associated with this event.  For use by SST Core only */
     inline LinkId_t getLinkId(void) const { return link_id; }
 
 
-    // Functor classes for Event handling
+    /// Functor classes for Event handling
     class HandlerBase {
     public:
-	virtual void operator()(Event*) = 0;
-	virtual ~HandlerBase() {}
+        /** Handler function */
+        virtual void operator()(Event*) = 0;
+        virtual ~HandlerBase() {}
     };
-    
 
+
+    /**
+     * Event Handler class with user-data argument
+     */
     template <typename classT, typename argT = void>
     class Handler : public HandlerBase {
     private:
-	typedef void (classT::*PtrMember)(Event*, argT);
-	classT* object;
-	const PtrMember member;
-	argT data;
-	
-    public:
-	Handler( classT* const object, PtrMember member, argT data ) :
-	    object(object),
-	    member(member),
-	    data(data)
-	{}
+        typedef void (classT::*PtrMember)(Event*, argT);
+        classT* object;
+        const PtrMember member;
+        argT data;
 
-	    void operator()(Event* event) {
-		(object->*member)(event,data);
-	    }
+    public:
+        /** Constructor
+         * @param object - Pointer to Object upon which to call the handler
+         * @param member - Member function to call as the handler
+         * @param data - Additional argument to pass to handler
+         */
+        Handler( classT* const object, PtrMember member, argT data ) :
+            object(object),
+            member(member),
+            data(data)
+        {}
+
+        void operator()(Event* event) {
+            (object->*member)(event,data);
+        }
     };
-    
+
+
+    /**
+     * Event Handler class with no user-data.
+     */
     template <typename classT>
     class Handler<classT, void> : public HandlerBase {
-    private:
-	typedef void (classT::*PtrMember)(Event*);
-	const PtrMember member;
-	classT* object;
-	
-    public:
-	Handler( classT* const object, PtrMember member ) :
-  	  member(member),
-	  object(object)
-	{}
+        private:
+            typedef void (classT::*PtrMember)(Event*);
+            const PtrMember member;
+            classT* object;
 
-	    void operator()(Event* event) {
-		(object->*member)(event);
-	    }
-    };
+        public:
+        /** Constructor
+         * @param object - Pointer to Object upon which to call the handler
+         * @param member - Member function to call as the handler
+         */
+            Handler( classT* const object, PtrMember member ) :
+                member(member),
+                object(object)
+            {}
 
+            void operator()(Event* event) {
+                (object->*member)(event);
+            }
+        };
+
+    /** Virtual function to "pretty-print" this event.  Should be implemented by subclasses. */
     virtual void print(const std::string& header, Output &out) const {
         out.output("%s Generic Event to be delivered at %" PRIu64 " with priority %d\n",
                 header.c_str(), getDeliveryTime(), getPriority());
     }
 
 protected:
+    /** Link used for delivery */
     Link* delivery_link;
-    
+
 private:
     LinkId_t link_id;
 
@@ -112,6 +140,9 @@ private:
 };
 
 
+/**
+ * Null Event.  Does nothing.
+ */
 class NullEvent : public Event {
 public:
     NullEvent() : Event() {}
