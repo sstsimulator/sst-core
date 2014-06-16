1b8a21a60be73ad6db5b60f01fd4f5b112d4b934
K. Scott Hemmert <kshemme@sandia.gov>
2014-06-16 18:01:49 +0000

    Redid indentation on file.  No other changes.

--- sst/core/sync.cc
+++ sst/core/sync.cc
@@ -27,11 +27,11 @@ namespace SST {
 
     Sync::Sync(TimeConverter* period) : Action()
     {
-	this->period = period;
-	Simulation *sim = Simulation::getSimulation();
-	SimTime_t next = sim->getCurrentSimCycle() + period->getFactor();
+        this->period = period;
+        Simulation *sim = Simulation::getSimulation();
+        SimTime_t next = sim->getCurrentSimCycle() + period->getFactor();
         setPriority(25);
-	sim->insertActivity( next, this );
+        sim->insertActivity( next, this );
     }
     
     Sync::~Sync()
@@ -41,34 +41,34 @@ namespace SST {
             delete i->second.second;
         }
         comm_map.clear();
-
+        
         for (link_map_t::iterator i = link_map.begin() ; i != link_map.end() ; ++i) {
             delete i->second;
         }
-	link_map.clear();
+        link_map.clear();
     }
     
     SyncQueue* Sync::registerLink(int rank, LinkId_t link_id, Link* link)
     {
-	SyncQueue* queue;
-	if ( comm_map.count(rank) == 0 ) {
+        SyncQueue* queue;
+        if ( comm_map.count(rank) == 0 ) {
             queue = comm_map[rank].first = new SyncQueue();
             comm_map[rank].second = new std::vector<Activity*>;
         } else {
             queue = comm_map[rank].first;
         }
 	
-	link_map[link_id] = link;
-	return queue;
+        link_map[link_id] = link;
+        return queue;
     }
 
     void
     Sync::finalizeLinkConfigurations() {
         for (link_map_t::iterator i = link_map.begin() ; i != link_map.end() ; ++i) {
-	    i->second->finalizeConfiguration();
+            i->second->finalizeConfiguration();
         }
     }
-
+    
     void
     Sync::execute(void)
     {
@@ -76,19 +76,19 @@ namespace SST {
         std::vector<boost::mpi::request> pending_requests;
 
         for (comm_map_t::iterator i = comm_map.begin() ; i != comm_map.end() ; ++i) {
-	   pending_requests.push_back(comm.isend(i->first, 0, *(i->second.first->getVector())));
-	   pending_requests.push_back(comm.irecv(i->first, 0, *(i->second.second)));
+            pending_requests.push_back(comm.isend(i->first, 0, *(i->second.first->getVector())));
+            pending_requests.push_back(comm.irecv(i->first, 0, *(i->second.second)));
         }
         boost::mpi::wait_all(pending_requests.begin(), pending_requests.end());
 
-	Simulation* sim = Simulation::getSimulation();
-	SimTime_t current_cycle = sim->getCurrentSimCycle();
-	// Clear the SyncQueue used to send the data
+        Simulation* sim = Simulation::getSimulation();
+        SimTime_t current_cycle = sim->getCurrentSimCycle();
+        // Clear the SyncQueue used to send the data
         for (comm_map_t::iterator i = comm_map.begin() ; i != comm_map.end() ; ++i) {
             i->second.first->clear();
 
-	    // Need to look through all the entries in the vector and
-	    // put them on the correct link
+            // Need to look through all the entries in the vector and
+            // put them on the correct link
             std::vector<Activity*> *tmp = i->second.second;
             for (std::vector<Activity*>::iterator j = tmp->begin() ; j != tmp->end() ; ++j) {
                 Event *ev = static_cast<Event*>(*j);
@@ -97,17 +97,17 @@ namespace SST {
                     printf("Link not found in map!\n");
                     abort();
                 } else {
-		    // Need to figure out what the "delay" is for this event.
-		    SimTime_t delay = ev->getDeliveryTime() - current_cycle;
+                    // Need to figure out what the "delay" is for this event.
+                    SimTime_t delay = ev->getDeliveryTime() - current_cycle;
                     link->second->send(delay,ev);
                 }
             }
-	    // Clear the receive vector
+            // Clear the receive vector
             tmp->clear();
         }        
 
-	SimTime_t next = sim->getCurrentSimCycle() + period->getFactor();
-	sim->insertActivity( next, this );
+        SimTime_t next = sim->getCurrentSimCycle() + period->getFactor();
+        sim->insertActivity( next, this );
 #endif
     }
 
@@ -116,7 +116,7 @@ namespace SST {
     {
 #ifdef HAVE_MPI
         std::vector<boost::mpi::request> pending_requests;
-
+        
         for (comm_map_t::iterator i = comm_map.begin() ; i != comm_map.end() ; ++i) {
             pending_requests.push_back(comm.isend(i->first, 0, i->second.first->getVector()));
             pending_requests.push_back(comm.irecv(i->first, 0, i->second.second));
@@ -124,11 +124,11 @@ namespace SST {
         boost::mpi::wait_all(pending_requests.begin(), pending_requests.end());
 
         for (comm_map_t::iterator i = comm_map.begin() ; i != comm_map.end() ; ++i) {
-	    // Clear the SyncQueue used to send the data
+            // Clear the SyncQueue used to send the data
             i->second.first->clear();
 
-	    // Need to look through all the entries in the vector and
-	    // put them on the correct link
+            // Need to look through all the entries in the vector and
+            // put them on the correct link
             std::vector<Activity*> *tmp = i->second.second;
             for (std::vector<Activity*>::iterator j = tmp->begin() ; j != tmp->end() ; ++j) {
                 Event *ev = static_cast<Event*>(*j);
@@ -137,20 +137,20 @@ namespace SST {
                     printf("Link not found in map!\n");
                     abort();
                 } else {
-		    // Need to figure out what the "delay" is for this event.
+                    // Need to figure out what the "delay" is for this event.
                     link->second->sendInitData_sync(ev);
                 }
             }
-	    // Clear the receive vector
+            // Clear the receive vector
             tmp->clear();
         }
 
-	// Do an allreduce to see if there were any messages sent
-	boost::mpi::communicator world;
-	int input = msg_count;
-	int count;
-	all_reduce( world, &input, 1, &count, std::plus<int>() );
-	return count;
+        // Do an allreduce to see if there were any messages sent
+        boost::mpi::communicator world;
+        int input = msg_count;
+        int count;
+        all_reduce( world, &input, 1, &count, std::plus<int>() );
+        return count;
 #else
         return 0;
 #endif
@@ -171,7 +171,7 @@ namespace SST {
         // don't serialize comm - let it be silently rebuilt at restart
         printf("end Sync::serialize\n");
     }
-
+    
 } // namespace SST
 
 
