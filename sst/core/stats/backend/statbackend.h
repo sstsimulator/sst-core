215334602fb43cd0264e13dd904622bfbf9c82ca
Si Hammond <simon.hammond@gmail.com>
2014-06-18 08:26:23 +0000

    Initial support for statistics backends (text, compressed text, HDF5 etc).

---
+++ sst/core/stats/backend/statbackend.h
@@ -0,0 +1,25 @@
+// Copyright 2009-2014 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2014, Sandia Corporation
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _H_SST_CORE_STATS_BASE_BACKEND
+#define _H_SST_CORE_STATS_BASE_BACKEND
+
+namespace SST {
+namespace Statistics {
+
+class StatisticsBackend {
+
+};
+
+}
+}
+
+#endif
