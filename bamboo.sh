ca3d1ac1e261277c43632e068617427928baae68
Cesar De La Paz <caesar.sst@gmail.com>
2014-07-14 15:00:54 +0000

    Changed variables and file name to use 'non-cacheable' instead of 'uncached' as this is is the proper lit termininology.  Other minor changes as well

--- bamboo.sh
+++ bamboo.sh
@@ -218,7 +218,7 @@ dotests() {
         ${SST_TEST_SUITES}/testSuite_diropenMP.sh
         ${SST_TEST_SUITES}/testSuite_stream.sh
         ${SST_TEST_SUITES}/testSuite_memHierarchy_bin.sh
-        ${SST_TEST_SUITES}/testSuite_uncache_openMP.sh
+        ${SST_TEST_SUITES}/testSuite_noncacheable_openMP.sh
 #        if [ $1 != "sstmainline_config_linux_with_ariel" -a $1 != "sstmainline_config_xml2python" ] ; then
 #            ${SST_TEST_SUITES}/testSuite_Sweep_openMP.sh
 #            ${SST_TEST_SUITES}/testSuite_dirSweep.sh
