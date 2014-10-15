ebd055c7507bbbad36411acbd75b410a3a2816ab
John Van Dyke <jpvandy@sandia.gov>
2014-10-15 17:47:26 +0000

    Add two VaultSim tests to the mainline nightly

--- bamboo.sh
+++ bamboo.sh
@@ -263,6 +263,7 @@ dotests() {
     ${SST_TEST_SUITES}/testSuite_check_maxrss.sh
     ${SST_TEST_SUITES}/testSuite_cassini_prefetch.sh
     ${SST_TEST_SUITES}/testSuite_messageGenerator.sh
+    ${SST_TEST_SUITES}/testSuite_VaultSim.sh
 
     HOST=`uname -n | awk -F. '{print $1}'`
 
