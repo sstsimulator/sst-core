8059a0943527a9cc3eecb71d37618b5c30b825d0
Si Hammond <simon.hammond@gmail.com>
2015-01-14 18:44:09 +0000

    SST string coversion functions

---
+++ sst/core/stringize.h
@@ -0,0 +1,75 @@
+
+#ifndef _H_SST_CORE_STRINGIZE
+#define _H_SST_CORE_STRINGIZE
+
+#include <sst_config.h>
+
+namespace SST {
+
+static std::string to_string(int val) {
+	char* buffer[32];
+	sprintf(buffer, "%d", val);
+
+	std::string buffer_str(buffer);
+	return buffer_str;
+};
+
+static std::string to_string(long val) {
+        char* buffer[32];
+        sprintf(buffer, "%l", val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+static std::string to_string(double val) {
+        char* buffer[32];
+        sprintf(buffer, "%f", val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+static std::string to_string(float val) {
+        char* buffer[32];
+        sprintf(buffer, "%f", val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+static std::string to_string(unsigned int val) {
+        char* buffer[32];
+        sprintf(buffer, "%u", val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+static std::string to_string(unsigned long val) {
+        char* buffer[32];
+        sprintf(buffer, "%lu", val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+static std::string to_string(uint32_t val) {
+        char* buffer[32];
+        sprintf(buffer, "%" PRIu32, val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+static std::string to_string(uint64_t val) {
+        char* buffer[32];
+        sprintf(buffer, "%" PRIu64, val);
+
+        std::string buffer_str(buffer);
+        return buffer_str;
+};
+
+}
+
+#define
