9fbdbd8a993a3492cbf1ed0e7813db6fb3f04ee1
Si Hammond <simon.hammond@gmail.com>
2015-01-14 20:57:03 +0000

    Adds SST types to stringize.

--- sst/core/stringize.h
+++ sst/core/stringize.h
@@ -7,30 +7,6 @@
 
 namespace SST {
 
-static std::string to_string(int val) {
-#ifdef HAVE_STDCXX_0X
-	return std::to_string(val);
-#else
-	char* buffer[32];
-	sprintf(buffer, "%d", val);
-
-	std::string buffer_str(buffer);
-	return buffer_str;
-#endif
-};
-
-static std::string to_string(long val) {
-#ifdef HAVE_STDCXX_0X
-	return std::to_string(val);
-#else
-        char* buffer[32];
-        sprintf(buffer, "%l", val);
-
-        std::string buffer_str(buffer);
-        return buffer_str;
-#endif
-};
-
 static std::string to_string(double val) {
 #ifdef HAVE_STDCXX_0X
 	return std::to_string(val);
@@ -55,24 +31,24 @@ static std::string to_string(float val) {
 #endif
 };
 
-static std::string to_string(unsigned int val) {
+static std::string to_string(int32_t val) {
 #ifdef HAVE_STDCXX_0X
 	return std::to_string(val);
 #else
         char* buffer[32];
-        sprintf(buffer, "%u", val);
+        sprintf(buffer, "%" PRId32, val);
 
         std::string buffer_str(buffer);
         return buffer_str;
 #endif
 };
 
-static std::string to_string(unsigned long val) {
+static std::string to_string(int64_t val) {
 #ifdef HAVE_STDCXX_0X
 	return std::to_string(val);
 #else
         char* buffer[32];
-        sprintf(buffer, "%lu", val);
+        sprintf(buffer, "%" PRId64, val);
 
         std::string buffer_str(buffer);
         return buffer_str;
