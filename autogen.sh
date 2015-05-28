e010a9fab18a27f8f07471b9cd752721d2e8c7fa
Aaron Levine <allevin@sandia.gov>
2015-05-28 20:23:11 +0000

    Changing autogen.sh to produce more readable output

--- autogen.sh
+++ autogen.sh
@@ -17,20 +17,24 @@ version=`echo $version | cut -f2 -d')'`
 version=`echo $version | cut -f1 -d' ' | sed -e 's/[A-Za-z]//g'`
 major_version=`echo $version | cut -f1 -d.`
 
-
 # do elemlib magic
 echo "Finding element libraries ..."
+ignored_list=
+added_make_list=
+added_m4_list=
 elemlib_list=
 elemlib_m4_list=
 for elemlib_dir in sst/elements/* ; do
   if test -d "$elemlib_dir" -a -r "$elemlib_dir/Makefile.am" ; then
     elemlib=`basename "$elemlib_dir"`
     if test -f "$elemlib_dir/.ignore" -a ! -f "$elemlib_dir/.unignore" ; then
-      echo " - ignoring element library $elemlib"
+#      echo " !- ignoring element library $elemlib"
+      ignored_list="$ignored_list$elemlib\n"
     elif test -f "$elemlib_dir/.ignore" && \
          test -s $elemlib_dir/.unignore && \
          test -z "`grep $USER $elemlib_dir/.unignore`" ; then
-      echo " - ignoring element library $elemlib"
+#      echo " !- ignoring element library $elemlib"
+      ignored_list="$ignored_list$elemlib\n"
     else
       if test -z "$elemlib_list" ; then
         elemlib_list="$elemlib"
@@ -38,13 +42,32 @@ for elemlib_dir in sst/elements/* ; do
         elemlib_list="$elemlib_list, $elemlib"
       fi
       if test -f "$elemlib_dir/configure.m4" ; then
-	echo " - including: $elemlib_dir/configure.m4"
+#        echo "  - including: $elemlib_dir/configure.m4"
+        added_m4_list="$added_m4_list$elemlib/config.m4\n"
         elemlib_m4_list="$elemlib_m4_list $elemlib_dir/configure.m4"
+      else 
+        added_make_list="$added_make_list$elemlib\n"
+#        echo "  - including element library: $elemlib_dir"
       fi
     fi
   fi
 done
 
+# Output our findings
+echo
+echo "Included Element Libraries in sst/elements/"
+echo "========================================================="
+echo $added_make_list
+echo "Included config.m4 files in sst/elements/"
+echo "========================================================="
+echo $added_m4_list
+echo "Ignored Element Libraries in sst/elements/"
+echo "========================================================="
+echo $ignored_list
+echo "========================================================="
+echo "========================================================="
+
+# Build the configuration file
 cat > "config/sst_m4_config_include.m4" <<EOF
 dnl -*- Autoconf -*-
 
