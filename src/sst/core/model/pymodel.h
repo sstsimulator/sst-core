b17f30d3f665a611fc2b7b35caa77c71a7716234
Aaron Levine <allevin@sandia.gov>
2016-05-19 14:15:55 -0600

    Update Copyrights

--- src/sst/core/model/pymodel.h
+++ src/sst/core/model/pymodel.h
@@ -1,10 +1,10 @@
 // -*- c++ -*-
 
-// Copyright 2009-2015 Sandia Corporation. Under the terms
+// Copyright 2009-2016 Sandia Corporation. Under the terms
 // of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
 // Government retains certain rights in this software.
 //
-// Copyright (c) 2009-2015, Sandia Corporation
+// Copyright (c) 2009-2016, Sandia Corporation
 // All rights reserved.
 //
 // This file is part of the SST software package. For license
