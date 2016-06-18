9c642a7c0d63dedea85b233431f9b315689153e8
feldergast <kshemme@sandia.gov>
2016-06-18 17:13:27 -0600

    Added serialization for Params and std::deque.
    Made Params serializable with the latest serialization interface.
    Added ability to serialize a std::deque.

--- src/sst/core/serialization/serialize.h
+++ src/sst/core/serialization/serialize.h
@@ -25,37 +25,47 @@ namespace Serialization {
 
 template <class T, class Enable = void>
 class serialize {
- public:
-  inline void operator()(T& t, serializer& ser){
-      // If the default gets called, then it's actually invalid
-      // because we don't know how to serialize it.
-
-      // This is a bit strange, but if I just do a
-      // static_assert(false) it always triggers, but if I use
-      // std::is_* then it seems to only trigger if something expands
-      // to this version of the template.
-      // static_assert(false,"Trying to serialize an object that is not serializable.");
-      static_assert(std::is_fundamental<T>::value,"Trying to serialize an object that is not serializable.");
-      static_assert(!std::is_fundamental<T>::value,"Trying to serialize an object that is not serializable.");
-  }
+public:
+    inline void operator()(T& t, serializer& ser){
+        // If the default gets called, then it's actually invalid
+        // because we don't know how to serialize it.
+        
+        // This is a bit strange, but if I just do a
+        // static_assert(false) it always triggers, but if I use
+        // std::is_* then it seems to only trigger if something expands
+        // to this version of the template.
+        // static_assert(false,"Trying to serialize an object that is not serializable.");
+        static_assert(std::is_fundamental<T>::value,"Trying to serialize an object that is not serializable.");
+        static_assert(!std::is_fundamental<T>::value,"Trying to serialize an object that is not serializable.");
+    }
 };
 
 template <class T>
 class serialize <T, typename std::enable_if<std::is_fundamental<T>::value || std::is_enum<T>::value>::type> {
- public:
-  inline void operator()(T& t, serializer& ser){
-      ser.primitive(t); 
-  }
+public:
+    inline void operator()(T& t, serializer& ser){
+        ser.primitive(t); 
+    }
+};
+
+// Maintains backward compatibility for now.  In the future, probably
+// need to do some error checking
+template <class U, class V>
+class serialize<std::pair<U,V> > {
+public:
+    inline void operator()(std::pair<U,V>& t, serializer& ser){
+        ser.primitive(t); 
+    }
 };
 
 template <>
 class serialize<bool> {
- public:
-  void operator()(bool &t, serializer& ser){
-    int bval = t;
-    ser.primitive(bval);
-    t = bool(bval);
-  }
+public:
+    void operator()(bool &t, serializer& ser){
+        int bval = t;
+        ser.primitive(bval);
+        t = bool(bval);
+    }
 };
 
 
@@ -70,6 +80,7 @@ operator&(serializer& ser, T& t){
 }
 
 #include <sst/core/serialization/serialize_array.h>
+#include <sst/core/serialization/serialize_deque.h>
 #include <sst/core/serialization/serialize_list.h>
 #include <sst/core/serialization/serialize_map.h>
 #include <sst/core/serialization/serialize_set.h>
