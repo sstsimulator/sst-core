8de07adc4553aa090b029613066af417c84a443f
Aaron Levine <allevin@sandia.gov>
2016-06-27 07:43:21 -0600

    Changed version to display the sha rather than the commit count.

--- src/sst/core/config.cc
+++ src/sst/core/config.cc
@@ -237,10 +237,10 @@ Config::parseCmdLine(int argc, char* argv[]) {
     print_timing = (var_map->count("print-timing-info") > 0);
 
     if ( var_map->count( "version" ) ) {
-        if (SSTCORE_GIT_COMMITCOUNT == PACKAGE_VERSION)
+        if (SSTCORE_GIT_HEADSHA == PACKAGE_VERSION)
             cout << "SST-Core Release Version (" PACKAGE_VERSION << ")" << endl;
         else
-            cout << "SST-Core Release Version (" PACKAGE_VERSION << ", Branch Commit Count: " SSTCORE_GIT_COMMITCOUNT << ")" << endl;
+            cout << "SST-Core Release Version (" PACKAGE_VERSION << ", Branch SHA: " SSTCORE_GIT_HEADSHA << ")" << endl;
         return 1;
     }
 
