6b284df15de96a0a1fc3e4ece27bd7da5ca27761
feldergast <kshemme@sandia.gov>
2017-04-26 08:00:08 -0600

    Changed deprecated warnings for old ELI.
    Changed the deprecated for old ELI to make clear that the old ELI
    is still the preferred API until 7.1, but the warning starts the
    promised one major release countdown to removal.
    
    Also added the ability for elements to turn off the warnings by
    using the correct #define to turn it off.  This can be used to
    turn off the warnings for our supported components, but have them
    show up on external components.

--- src/sst/core/element.h
+++ src/sst/core/element.h
@@ -121,10 +121,14 @@ struct ElementInfoGenerator {
 
 /** Describes all the parts of the Element Library
  */
+#ifdef SST_ELI_COMPILE_OLD_ELI_WITHOUT_DEPRECATION_WARNINGS
+struct ElementLibraryInfo {
+#else
 #if SST_BUILDING_CORE
 struct ElementLibraryInfo {
 #else
-struct __attribute__ ((deprecated("Please use new ELI defined in sst/core/elementinfo.h"))) ElementLibraryInfo {
+struct __attribute__ ((deprecated("Old ELI support (defined in sst/core/element.h) will be removed in version 8.0, but is still the preferred API until version 7.1.  New ELI support (defined in sst/core/elementinfo.h) is available, but experimental and subject to change until version 7.1."))) ElementLibraryInfo {
+#endif
 #endif
     const char *name;										/*!< Name of the Library. */
     const char *description;								/*!< Brief description of the Library */
