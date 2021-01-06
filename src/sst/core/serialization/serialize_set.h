d7fde3edd0184bd64b50514fa0b8bb02510717a3
feldergast <kshemme@sandia.gov>
2021-01-06 12:38:44 -0700

    Added serialization for unordered_map and unordered_set.

--- src/sst/core/serialization/serialize_set.h
+++ src/sst/core/serialization/serialize_set.h
@@ -13,6 +13,7 @@
 #define SERIALIZE_SET_H
 
 #include <set>
+#include <unordered_set>
 #include "sst/core/serialization/serializer.h"
 
 namespace SST {
@@ -21,46 +22,88 @@ namespace Serialization {
 
 template <class T>
 class serialize<std::set<T> > {
-  typedef std::set<T> Set;
+    typedef std::set<T> Set;
 
 public:
-  void
-  operator()(Set& v, serializer& ser) {
-    typedef typename std::set<T>::iterator iterator;
-    switch(ser.mode())
-    {
-    case serializer::SIZER: {
-      size_t size = v.size();
-      ser.size(size);
-      iterator it, end = v.end();
-      for (it=v.begin(); it != end; ++it){
-        T& t = const_cast<T&>(*it);
-        serialize<T>()(t,ser);
-      }
-      break;
+    void
+    operator()(Set& v, serializer& ser) {
+        typedef typename std::set<T>::iterator iterator;
+        switch(ser.mode()) {
+        case serializer::SIZER: {
+            size_t size = v.size();
+            ser.size(size);
+            iterator it, end = v.end();
+            for (it=v.begin(); it != end; ++it){
+                T& t = const_cast<T&>(*it);
+                serialize<T>()(t,ser);
+            }
+            break;
+        }
+        case serializer::PACK: {
+            size_t size = v.size();
+            ser.pack(size);
+            iterator it, end = v.end();
+            for (it=v.begin(); it != end; ++it){
+                T& t = const_cast<T&>(*it);
+                serialize<T>()(t,ser);
+            }
+            break;
+        }
+        case serializer::UNPACK: {
+            size_t size;
+            ser.unpack(size);
+            for (size_t i=0; i < size; ++i){
+                T t;
+                serialize<T>()(t,ser);
+                v.insert(t);
+            }
+            break;
+        }
+        }
     }
-    case serializer::PACK: {
-      size_t size = v.size();
-      ser.pack(size);
-      iterator it, end = v.end();
-      for (it=v.begin(); it != end; ++it){
-        T& t = const_cast<T&>(*it);
-        serialize<T>()(t,ser);
-      }
-      break;
+};
+
+template <class T>
+class serialize<std::unordered_set<T> > {
+    typedef std::unordered_set<T> Set;
+
+public:
+    void
+    operator()(Set& v, serializer& ser) {
+        typedef typename std::unordered_set<T>::iterator iterator;
+        switch(ser.mode()) {
+        case serializer::SIZER: {
+            size_t size = v.size();
+            ser.size(size);
+            iterator it, end = v.end();
+            for (it=v.begin(); it != end; ++it){
+                T& t = const_cast<T&>(*it);
+                serialize<T>()(t,ser);
+            }
+            break;
+        }
+        case serializer::PACK: {
+            size_t size = v.size();
+            ser.pack(size);
+            iterator it, end = v.end();
+            for (it=v.begin(); it != end; ++it){
+                T& t = const_cast<T&>(*it);
+                serialize<T>()(t,ser);
+            }
+            break;
+        }
+        case serializer::UNPACK: {
+            size_t size;
+            ser.unpack(size);
+            for (size_t i=0; i < size; ++i){
+                T t;
+                serialize<T>()(t,ser);
+                v.insert(t);
+            }
+            break;
+        }
+        }
     }
-    case serializer::UNPACK: {
-      size_t size;
-      ser.unpack(size);
-      for (size_t i=0; i < size; ++i){
-        T t;
-        serialize<T>()(t,ser);
-        v.insert(t);
-      }
-      break;
-    }
-    }
-  }
 };
 
 }
