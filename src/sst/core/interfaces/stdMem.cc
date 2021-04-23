e847b0a74d865e13843002f551d798e97686b20d
Gwen <grvosku@sandia.gov>
2021-04-23 10:44:41 -0600

    First cut at new StandardMem interface

---
+++ src/sst/core/interfaces/stdMem.cc
@@ -0,0 +1,16 @@
+// -*- mode: c++ -*-
+// Copyright 2009-2021 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2021, NTESS
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+//
+#include "sst_config.h"
+#include "sst/core/interfaces/stdMem.h"
+
+std::atomic<SST::Experimental::Interfaces::StandardMem::Request::id_t> SST::Experimental::Interfaces::StandardMem::Request::main_id(0);
