5e06685c8f64138ed68ffdf87d7af38d2579d6f9
Aaron Levine <allevin@sandia.gov>
2021-07-27 11:58:35 -0600

    Fixed logic bug, and cleaned up formatting

--- scripts/clang-format-test.sh
+++ scripts/clang-format-test.sh
@@ -13,63 +13,55 @@ pwd
 DIRS_TO_SKIP="-path ./build "
 DIRS_TO_SKIP="$DIRS_TO_SKIP -or -path ./src/sst/core/libltdl"
 DIRS_TO_SKIP="$DIRS_TO_SKIP -or -path ./src/sst/core/tinyxml"
+# Add additional directories to skip here...
 
-echo "====================================="
-echo "=== PERFORMING CLANG-FORMAT TESTING ="
-echo "====================================="
+echo "======================================="
+echo "=== PERFORMING CLANG-FORMAT TESTING ==="
+echo "======================================="
 
 # Run clang-format on all specific .h files
 echo
-echo "=== CLANG-FORMAT TESTING *.h FILES"
 find . -type d \( $DIRS_TO_SKIP \) -prune -false -o -name '*.h' -exec clang-format --dry-run {} \;  > clang_format_results_h.txt 2>&1
 rtncode=$?
 echo "=== CLANG-FORMAT FINISHED *.h CHECKS WITH RTN CODE $rtncode"
 
 # Run clang-format on all specific .cc files
 echo
-echo "=== CLANG-FORMAT TESTING *.cc FILES"
 find . -type d \( $DIRS_TO_SKIP \) -prune -false -o -name '*.cc' -exec clang-format --dry-run {} \; > clang_format_results_cc.txt 2>&1
 rtncode=$?
 echo "=== CLANG-FORMAT FINISHED *.cc CHECKS WITH RTN CODE $rtncode"
 
-echo
-echo "==========================================="
-echo "=== REVIEWING CLANG-FORMAT RESULTS FILES =="
-echo "==========================================="
-echo
-
 # Set a test result return to a default rtn
 export FINAL_TEST_RESULT=0
 
+# Evaluate the Results
 echo
-if [ -s clang_format_results_h.txt ]; then
-  echo "=== CLANG FORMAT RESULTS FOR .h FILES IS IS NOT EMPTY - FAILURE"
-  cat clang_format_results_h.txt
+if [ -s ./clang_format_results_h.txt ]; then
+  echo "=== CLANG FORMAT RESULT FILE FOR .h FILES IS IS NOT EMPTY - FAILURE"
+  cat ./clang_format_results_h.txt
   export FINAL_TEST_RESULT=1
 else
-  echo "=== CLANG FORMAT RESULTS FOR .h FILES IS EMPTY - SUCCESS"
+  echo "=== CLANG FORMAT RESULT FILE FOR .h FILES IS EMPTY - SUCCESS"
 fi
 
 echo
-if [ -s clang_format_results_cc.txt ]; then
-  echo "=== CLANG FORMAT RESULTS FOR .cc FILES IS IS NOT EMPTY - FAILURE"
-  cat clang_format_results_cc.txt
+if [ -s ./clang_format_results_cc.txt ]; then
+  echo "=== CLANG FORMAT RESULT FILE FOR .cc FILES IS IS NOT EMPTY - FAILURE"
+  cat ./clang_format_results_cc.txt
   export FINAL_TEST_RESULT=1
 else
-  echo "=== CLANG FORMAT RESULTS FOR .cc FILES IS EMPTY - SUCCESS"
+  echo "=== CLANG FORMAT RESULT FILE FOR .cc FILES IS EMPTY - SUCCESS"
 fi
 
-
-
+# Display the final results
 echo
-echo "====================================="
+echo "========================================"
 if [ $FINAL_TEST_RESULT == 0 ]; then
-echo "=== FINAL TEST RESULT = ($FINAL_TEST_RESULT) - FAILED"
+echo "=== FINAL TEST RESULT = ($FINAL_TEST_RESULT) - PASSED ==="
 else
-echo "=== FINAL TEST RESULT = ($FINAL_TEST_RESULT) - PASSED"
+echo "=== FINAL TEST RESULT = ($FINAL_TEST_RESULT) - FAILED ==="
 fi
-echo "====================================="
+echo "========================================"
 echo
-
 
 exit $FINAL_TEST_RESULT
