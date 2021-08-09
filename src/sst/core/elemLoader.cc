9333b77cbc367691bd6b4907248eb34678cecfa7
Simon David Hammond <sdhammo@sst-devel-new.srn.sandia.gov>
2021-08-09 13:33:57 -0600

    Fix formatting for clang-format test

--- src/sst/core/elemLoader.cc
+++ src/sst/core/elemLoader.cc
@@ -84,9 +84,7 @@ ElemLoader::loadLibrary(const std::string& elemlib, std::ostream& err_os)
     for ( std::string const& next_path : paths ) {
         if ( verbose ) { printf("SST-DL: Searching: %s\n", next_path.c_str()); }
 
-        if ( next_path.back() == '/' ) {
-            sprintf(full_path, "%slib%s.so", next_path.c_str(), elemlib.c_str());
-        }
+        if ( next_path.back() == '/' ) { sprintf(full_path, "%slib%s.so", next_path.c_str(), elemlib.c_str()); }
         else {
             sprintf(full_path, "%s/lib%s.so", next_path.c_str(), elemlib.c_str());
         }
@@ -103,9 +101,7 @@ ElemLoader::loadLibrary(const std::string& elemlib, std::ostream& err_os)
         // this implies ordering of .so before .dylib in priority.
 
         if ( nullptr == handle ) {
-            if ( next_path.back() == '/' ) {
-                sprintf(full_path, "%slib%s.dylib", next_path.c_str(), elemlib.c_str());
-            }
+            if ( next_path.back() == '/' ) { sprintf(full_path, "%slib%s.dylib", next_path.c_str(), elemlib.c_str()); }
             else {
                 sprintf(full_path, "%s/lib%s.dylib", next_path.c_str(), elemlib.c_str());
             }
