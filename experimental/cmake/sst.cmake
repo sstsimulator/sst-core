e37e639d9817735cdda80dc8d4dfccad2533f6c2
Cannada Lewis <canlewi@localhost.localdomain>
2021-09-01 11:57:11 -0400

    Added a cmake variable to hold the top level src dir

--- experimental/cmake/sst.cmake
+++ experimental/cmake/sst.cmake
@@ -143,14 +143,14 @@ endif()
 
 find_package(Git REQUIRED)
 execute_process(
-  COMMAND ${GIT_EXECUTABLE} --git-dir=${sst-core_SOURCE_DIR}/../.git rev-parse
+  COMMAND ${GIT_EXECUTABLE} --git-dir=${SST_TOP_SRC_DIR}/.git rev-parse
           HEAD
   RESULT_VARIABLE HASH_RESULT
   OUTPUT_VARIABLE SSTCORE_GIT_HEADSHA
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 execute_process(
-  COMMAND ${GIT_EXECUTABLE} --git-dir=${sst-core_SOURCE_DIR}/../.git rev-parse
+  COMMAND ${GIT_EXECUTABLE} --git-dir=${SST_TOP_SRC_DIR}/.git rev-parse
           --abbrev-ref HEAD
   RESULT_VARIABLE BRANCH_RESULT
   OUTPUT_VARIABLE SSTCORE_GIT_BRANCH
