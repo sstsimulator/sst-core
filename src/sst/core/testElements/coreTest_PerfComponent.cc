2388953f4c9593a87b0bfc08222595518911d2c9
David Donofrio <ddonofrio@tactcomplabs.com>
2021-09-23 16:29:29 -0700

    preliminary test created. also added a PerfComponent to test the basic performance testing functionality

---
+++ src/sst/core/testElements/coreTest_PerfComponent.cc
@@ -0,0 +1,171 @@
+// Copyright 2009-2021 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2021, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#include "sst_config.h"
+
+#include "sst/core/testElements/coreTest_PerfComponent.h"
+
+#include "sst/core/event.h"
+#include "sst/core/simulation.h"
+#include "sst/core/testElements/coreTest_ComponentEvent.h"
+
+#include <assert.h>
+
+using namespace SST;
+using namespace SST::CoreTestComponent;
+
+coreTestPerfComponent::coreTestPerfComponent(SST::ComponentId_t id, SST::Params& params) : coreTestPerfComponentBase2(id)
+{
+    bool found;
+
+    rng = new SST::RNG::MarsagliaRNG(11, 272727);
+
+    // get parameters
+    workPerCycle = params.find<int64_t>("workPerCycle", 0, found);
+    if ( !found ) {
+        Simulation::getSimulation()->getSimulationOutput().fatal(CALL_INFO, -1, "couldn't find work per cycle\n");
+    }
+
+    commFreq = params.find<int64_t>("commFreq", 0, found);
+    if ( !found ) {
+        Simulation::getSimulation()->getSimulationOutput().fatal(
+            CALL_INFO, -1, "couldn't find communication frequency\n");
+    }
+
+    commSize = params.find<int64_t>("commSize", 16);
+
+    // init randomness
+    srand(1);
+    neighbor = rng->generateNextInt32() % 4;
+
+    // tell the simulator not to end without us
+    registerAsPrimaryComponent();
+    primaryComponentDoNotEndSim();
+
+    // configure out links
+    N = configureLink("Nlink", new Event::Handler<coreTestPerfComponent>(this, &coreTestPerfComponent::handleEvent));
+    S = configureLink("Slink", new Event::Handler<coreTestPerfComponent>(this, &coreTestPerfComponent::handleEvent));
+    E = configureLink("Elink", new Event::Handler<coreTestPerfComponent>(this, &coreTestPerfComponent::handleEvent));
+    W = configureLink("Wlink", new Event::Handler<coreTestPerfComponent>(this, &coreTestPerfComponent::handleEvent));
+
+    countN = registerStatistic<int>("N");
+    countS = registerStatistic<int>("S");
+    countE = registerStatistic<int>("E");
+    countW = registerStatistic<int>("W");
+
+    assert(N);
+    assert(S);
+    assert(E);
+    assert(W);
+
+    // set our clock
+    auto clockHandler = new Clock::Handler<coreTestPerfComponent>(this, &coreTestPerfComponent::clockTic);
+    SST::TimeConverter* timeConverter = registerClock("1GHz", clockHandler);
+    registerClockHandler(clockHandler);
+
+}
+
+coreTestPerfComponent::~coreTestPerfComponent()
+{
+    delete rng;
+}
+
+coreTestPerfComponent::coreTestPerfComponent() : coreTestPerfComponentBase2(-1)
+{
+    // for serialization only
+}
+
+// incoming events are scanned and deleted
+void
+coreTestPerfComponent::handleEvent(Event* ev)
+{
+    // printf("recv\n");
+    coreTestComponentEvent* event = dynamic_cast<coreTestComponentEvent*>(ev);
+    if ( event ) {
+        // scan through each element in the payload and do something to it
+        volatile int sum = 0;
+        for ( coreTestComponentEvent::dataVec::iterator i = event->payload.begin(); i != event->payload.end(); ++i ) {
+            sum += *i;
+        }
+        delete event;
+    }
+    else {
+        printf("Error! Bad Event Type!\n");
+    }
+}
+
+// each clock tick we do 'workPerCycle' iterations of a coreTest loop.
+// We have a 1/commFreq chance of sending an event of size commSize to
+// one of our neighbors.
+bool coreTestPerfComponent::clockTic(Cycle_t)
+{
+    // do work
+    // loop becomes:
+    /*  00001ab5        movl    0xe0(%ebp),%eax
+        00001ab8        incl    %eax
+        00001ab9        movl    %eax,0xe0(%ebp)
+        00001abc        incl    %edx
+        00001abd        cmpl    %ecx,%edx
+        00001abf        jne     0x00001ab5
+
+        6 instructions.
+    */
+
+    volatile int v = 0;
+    for ( int i = 0; i < workPerCycle; ++i ) {
+        v++;
+    }
+
+    // communicate?
+    if ( (rng->generateNextInt32() % commFreq) == 0 ) {
+        // yes, communicate
+        // create event
+        coreTestComponentEvent* e = new coreTestComponentEvent();
+        // fill payload with commSize bytes
+        for ( int i = 0; i < (commSize); ++i ) {
+            e->payload.push_back(1);
+        }
+        // find target
+        neighbor = neighbor + 1;
+        neighbor = neighbor % 4;
+        // send
+        switch ( neighbor ) {
+        case 0:
+            N->send(e);
+            countN->addData(1);
+            break;
+        case 1:
+            S->send(e);
+            countS->addData(1);
+            break;
+        case 2:
+            E->send(e);
+            countE->addData(1);
+            break;
+        case 3:
+            W->send(e);
+            countW->addData(1);
+            break;
+        default:
+            printf("bad neighbor\n");
+        }
+        // printf("sent\n");
+    }
+
+    // return false so we keep going
+    return false;
+}
+
+// Element Libarary / Serialization stuff
---
+++ src/sst/core/testElements/coreTest_PerfComponent.h
@@ -0,0 +1,137 @@
+// Copyright 2009-2021 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2021, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef SST_CORE_CORETEST_PERF_COMPONENT_H
+#define SST_CORE_CORETEST_PERF_COMPONENT_H
+
+#include <sst/core/component.h>
+#include <sst/core/link.h>
+#include <sst/core/rng/marsaglia.h>
+
+namespace SST {
+namespace CoreTestComponent {
+
+// These first two classes are just base classes to test ELI
+// inheritance.  The definition of the ELI items are spread through 2
+// component base classes to make sure they get inherited in the
+// actual component that can be instanced.
+class coreTestPerfComponentBase : public SST::Component
+{
+public:
+    SST_ELI_REGISTER_COMPONENT_BASE(SST::CoreTestComponent::coreTestPerfComponentBase)
+
+    SST_ELI_DOCUMENT_PARAMS(
+        { "workPerCycle", "Count of busy work to do during a clock tick.", NULL}
+    )
+
+    SST_ELI_DOCUMENT_STATISTICS(
+        { "N", "events sent on N link", "counts", 1 }
+    )
+
+    SST_ELI_DOCUMENT_PORTS(
+        {"Nlink", "Link to the coreTestComponent to the North", { "coreTestComponent.coreTestComponentEvent", "" } }
+    )
+
+    coreTestPerfComponentBase(ComponentId_t id) : SST::Component(id) {}
+    ~coreTestPerfComponentBase() {}
+};
+
+class coreTestPerfComponentBase2 : public coreTestPerfComponentBase
+{
+public:
+    SST_ELI_REGISTER_COMPONENT_DERIVED_BASE(
+        SST::CoreTestComponent::coreTestPerfComponentBase2, SST::CoreTestComponent::coreTestPerfComponentBase)
+
+    SST_ELI_DOCUMENT_PARAMS(
+        { "commFreq",     "Approximate frequency of sending an event during a clock tick.", NULL},
+    )
+
+    SST_ELI_DOCUMENT_STATISTICS(
+        { "S", "events sent on S link", "counts", 1 }
+    )
+
+    SST_ELI_DOCUMENT_PORTS(
+        {"Slink", "Link to the coreTestComponent to the South", { "coreTestComponent.coreTestComponentEvent", "" } }
+    )
+
+    coreTestPerfComponentBase2(ComponentId_t id) : coreTestPerfComponentBase(id) {}
+    ~coreTestPerfComponentBase2() {}
+};
+
+class coreTestPerfComponent : public coreTestPerfComponentBase2
+{
+public:
+    // REGISTER THIS COMPONENT INTO THE ELEMENT LIBRARY
+    SST_ELI_REGISTER_COMPONENT(
+        coreTestPerfComponent,
+        "coreTestElement",
+        "coreTestPerfComponent",
+        SST_ELI_ELEMENT_VERSION(1,0,0),
+        "CoreTest Test Perf Component",
+        COMPONENT_CATEGORY_PROCESSOR
+    )
+
+    SST_ELI_DOCUMENT_PARAMS(
+        { "commSize",     "Size of communication to send.", "16"}
+    )
+
+    SST_ELI_DOCUMENT_STATISTICS(
+        { "E", "events sent on E link", "counts", 1 },
+        { "W", "events sent on W link", "counts", 1 }
+    )
+
+    SST_ELI_DOCUMENT_PORTS(
+        {"Elink", "Link to the coreTestComponent to the East",  { "coreTestComponent.coreTestComponentEvent", "" } },
+        {"Wlink", "Link to the coreTestComponent to the West",  { "coreTestComponent.coreTestComponentEvent", "" } }
+    )
+
+    // Optional since there is nothing to document
+    SST_ELI_DOCUMENT_SUBCOMPONENT_SLOTS(
+    )
+
+    coreTestPerfComponent(SST::ComponentId_t id, SST::Params& params);
+    ~coreTestPerfComponent();
+
+    void setup() {}
+    void finish() { printf("Perf Test Component Finished.\n"); }
+
+private:
+    coreTestPerfComponent();                         // for serialization only
+    coreTestPerfComponent(const coreTestPerfComponent&); // do not implement
+    void operator=(const coreTestPerfComponent&);    // do not implement
+
+    void         handleEvent(SST::Event* ev);
+    virtual bool clockTic(SST::Cycle_t);
+
+    int workPerCycle;
+    int commFreq;
+    int commSize;
+    int neighbor;
+
+    SST::RNG::MarsagliaRNG*          rng;
+    SST::Link*                       N;
+    SST::Link*                       S;
+    SST::Link*                       E;
+    SST::Link*                       W;
+    SST::Statistics::Statistic<int>* countN;
+    SST::Statistics::Statistic<int>* countS;
+    SST::Statistics::Statistic<int>* countE;
+    SST::Statistics::Statistic<int>* countW;
+};
+
+} // namespace CoreTestPerfComponent
+} // namespace SST
+
+#endif // SST_CORE_CORETEST_PERF_COMPONENT_H
