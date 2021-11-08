e3261d5c8b43c88c6bedc891ade3bb4c016f04c4
jleidel <jleidel@users.noreply.github.com>
2021-11-08 09:17:54 -0600

    Adding JSON model reader infrastructure (#751)
    * adding json reader infrastructure to match the json writer infrastructure in Pull Request #748

---
+++ src/sst/core/model/json/jsonmodel.h
@@ -0,0 +1,125 @@
+// -*- c++ -*-
+
+// Copyright 2009-2021 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2021, NTESS
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef SST_CORE_MODEL_JSON_JSONMODEL_H
+#define SST_CORE_MODEL_JSON_JSONMODEL_H
+
+#include "sst_config.h"
+
+#include "sst/core/component.h"
+#include "sst/core/config.h"
+#include "sst/core/configGraph.h"
+#include "sst/core/cputimer.h"
+#include "sst/core/factory.h"
+#include "sst/core/memuse.h"
+#include "sst/core/model/sstmodel.h"
+#include "sst/core/output.h"
+#include "sst/core/rankInfo.h"
+#include "sst/core/simulation.h"
+#include "sst/core/sst_types.h"
+#include "sst/core/warnmacros.h"
+
+#include "nlohmann/json.hpp"
+
+#include <fstream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace SST;
+using json = nlohmann::json;
+
+namespace SST {
+namespace Core {
+
+class SSTJSONModelDefinition : public SSTModelDescription
+{
+public:
+    SST_ELI_REGISTER_MODEL_DESCRIPTION(
+          SST::Core::SSTJSONModelDefinition,
+          "sst",
+          "model.json",
+          SST_ELI_ELEMENT_VERSION(1,0,0),
+          "JSON model for building SST simulation graphs",
+          true)
+
+    SST_ELI_DOCUMENT_MODEL_SUPPORTED_EXTENSIONS(".json")
+
+    SSTJSONModelDefinition(const std::string& script_file, int verbosity, Config* config, double start_time);
+    virtual ~SSTJSONModelDefinition();
+
+    ConfigGraph* createConfigGraph() override;
+
+protected:
+    std::string   scriptName;
+    Output*       output;
+    Config*       config;
+    ConfigGraph*  graph;
+    ComponentId_t nextComponentId;
+    double        start_time;
+
+private:
+#define SST_MODEL_JSON_NAME_IDX            0
+#define SST_MODEL_JSON_COMPONENTID_IDX     1
+#define SST_MODEL_JSON_CONFIGCOMPONENT_IDX 2
+
+    typedef std::vector<std::tuple<std::string, ComponentId_t, ConfigComponent*>> CompTuple;
+    CompTuple                                                                     configGraphTuple;
+
+    void          recursiveSubcomponent(ConfigComponent* Parent, nlohmann::basic_json<> compArray);
+    void          discoverProgramOptions(json jFile);
+    void          discoverComponents(json jFile);
+    void          discoverLinks(json jFile);
+    void          discoverGlobalParams(json jFile);
+    ComponentId_t findComponentIdByName(std::string Name);
+
+    /// Contains a vector of all the known global configuration options
+    /// This list is derived from ~/src/sst/core/config.cc
+    const std::vector<std::string> programOpts = { "help",
+                                                   "verbose",
+                                                   "version",
+                                                   "disable-signal-handlers",
+                                                   "no-env-config",
+                                                   "print-timing-info",
+                                                   "print-env",
+                                                   "sdl-file",
+                                                   "stopAtCycle",
+                                                   "stopAfter",
+                                                   "debug-file",
+                                                   "lib-path",
+                                                   "add-lib-path",
+                                                   "run-mode",
+                                                   "stop-at",
+                                                   "heartbeat-period",
+                                                   "timebase",
+                                                   "partitioner",
+                                                   "timeVortex",
+                                                   "output-directory",
+                                                   "output-config",
+                                                   "output-dot",
+                                                   "dot-verbosity",
+                                                   "output-xml",
+                                                   "output-json",
+                                                   "output-partition",
+                                                   "output-prefix-core",
+#ifdef USE_MEMPOOL
+                                                   "output-undeleted-core",
+#endif
+                                                   "model-options",
+                                                   "num-threads" };
+};
+
+} // namespace Core
+} // namespace SST
+
+#endif // SST_CORE_MODEL_JSON_JSONMODEL_H
