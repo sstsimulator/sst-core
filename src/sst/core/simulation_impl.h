6d4cb18973fcdd144fd69e8d65b41fa5e381e6b2
jleidel <jleidel@users.noreply.github.com>
2022-05-19 10:51:03 -0500

    updating Fix for Issue #808; fixing issues in model crashes withe perf-tracking and profiling are enabled (#856)
    Co-authored-by: John Leidel <jleidel@tactcomplabs.com>

--- src/sst/core/simulation_impl.h
+++ src/sst/core/simulation_impl.h
@@ -299,6 +299,16 @@ public:
     /** Return the Statistic Processing Engine associated with this Simulation */
     Statistics::StatisticProcessingEngine* getStatisticsProcessingEngine(void) const;
 
+#if SST_EVENT_PROFILING
+private:
+    typedef std::map<std::string, uint64_t> eventCounter_t;
+    void incrementEventCounter(const std::string& component, Simulation_impl::eventCounter_t& counters);
+
+public:
+    void incrementEventCounters(const std::string& sendComponent, const std::string& recvComponent);
+    void incrementEventHandlerTime(const std::string& component, uint64_t count);
+#endif
+
     // private:
 
     friend class Link;
@@ -404,12 +414,12 @@ public:
 #endif
 
 #if SST_EVENT_PROFILING
-    uint64_t                        rankLatency         = 0;
-    uint64_t                        rankExchangeCounter = 0;
-    std::map<std::string, uint64_t> eventHandlers;
-    std::map<std::string, uint64_t> eventRecvCounters;
-    std::map<std::string, uint64_t> eventSendCounters;
-    uint64_t                        messageXferSize = 0;
+    uint64_t       rankLatency         = 0;
+    uint64_t       rankExchangeCounter = 0;
+    eventCounter_t eventHandlers;
+    eventCounter_t eventRecvCounters;
+    eventCounter_t eventSendCounters;
+    uint64_t       messageXferSize = 0;
 #endif
 
 #if SST_SYNC_PROFILING
