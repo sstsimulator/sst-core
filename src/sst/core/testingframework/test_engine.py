3d906d3dc39ea91915e1e52d776c41b5988a8063
grvosku <grvosku@sandia.gov>
2023-04-10 12:57:19 -0600

    Copyright update Also some comment fixes in rng

--- src/sst/core/testingframework/test_engine.py
+++ src/sst/core/testingframework/test_engine.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-## Copyright 2009-2022 NTESS. Under the terms
-## of Contract DE-NA0003525 with NTESS, the U.S.
-## Government retains certain rights in this software.
-##
-## Copyright (c) 2009-2022, NTESS
-## All rights reserved.
-##
-## This file is part of the SST software package. For license
-## information, see the LICENSE file in the top level directory of the
-## distribution.
+# Copyright 2009-2023 NTESS. Under the terms
+# of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+#
+# Copyright (c) 2009-2023, NTESS
+# All rights reserved.
+#
+# This file is part of the SST software package. For license
+# information, see the LICENSE file in the top level directory of the
+# distribution.
 
 """ This module is the main testing engine.  It will init testing variables,
     parse the cmd line vars, read the sstsimulator.conf file to find where
