5d51df41cb80a9140b4e56cf26e9d6b0ca10f5cb
Scott Hemmert <kshemme@sandia.gov>
2024-06-20 14:53:14 -0600

    Modified TraceFunction object to require that an environment variable be set to enable output.  This makes it possible to leave the TraceFunction calls in while developing, but still be able to run and pass testing. Update TraceFunction for better formatting.

---
+++ tests/test_Output.py
@@ -0,0 +1,26 @@
+# Copyright 2009-2024 NTESS. Under the terms
+# of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+#
+# Copyright (c) 2009-2024, NTESS
+# All rights reserved.
+#
+# This file is part of the SST software package. For license
+# information, see the LICENSE file in the top level directory of the
+# distribution.
+import sst
+import sys
+
+sst.setProgramOption("stop-at", "1us");
+
+ranks = sst.getMPIRankCount();
+threads = sst.getThreadCount();
+
+test = sys.argv[1]
+
+num_components = ranks * threads
+
+for x in range(num_components):
+    comp = sst.Component("Component{0}".format(x), "coreTestElement.coreTestOutput")
+    comp.addParam("test", test)
+
