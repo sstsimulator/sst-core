3398ba8bb40af55a9f4336a2305421af6da61b84
grvosku <grvosku@sandia.gov>
2024-07-15 09:42:44 -0600

    Fix test for when hdf5 is not present

--- tests/testsuite_default_StatisticsComponent.py
+++ tests/testsuite_default_StatisticsComponent.py
@@ -82,6 +82,7 @@ class testcase_StatisticComponent(SSTTestCase):
             options = "--model-options=\"hdf5\""
             ref_group_stat_file_h5 = "{0}/refFiles/test_StatisticsComponent_{1}_group_stats.h5".format(testsuitedir, testtype)
             out_group_stat_file_h5 = "{0}/test_StatisticsComponent_{1}_group_stats.h5".format(outdir, testtype)
+            reffile = "{0}/refFiles/test_StatisticsComponent_{1}_h5.out".format(testsuitedir, testtype)
         
         # Perform the test
         self.run_sst(sdlfile, outfile, other_args=options)
