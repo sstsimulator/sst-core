248e0be34787ca4918db3032907f52a07ae9d70a
grvosku <grvosku@sandia.gov>
2025-01-16 09:33:09 -0700

    Fix bug in sst-info filtering

--- src/sst/core/sstinfo.cc
+++ src/sst/core/sstinfo.cc
@@ -1027,7 +1027,7 @@ SSTLibraryInfo::outputHumanReadable(std::ostream& os, bool printAll)
                 // false, but shouldPrintElement() is true)
                 bool is_alias = (pair.second->getAlias() == pair.first);
                 bool print    = (!is_alias && printAll) ||
-                             (is_alias && !printAll && shouldPrintElement(getLibraryName(), pair.first));
+                             (!printAll && shouldPrintElement(getLibraryName(), pair.first));
 
                 if ( print ) {
                     os << "   " << BaseType::ELI_baseName() << " " << idx << ": " << pair.first << "\n";
