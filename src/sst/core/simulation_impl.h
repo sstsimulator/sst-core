143ee65a24633172723cbf5652ac6f827a026981
Shannon Kuntz <skuntz@tactcomplabs.com>
2025-04-22 08:35:00 -0500

    Add support for shutdown from interactive console

--- src/sst/core/simulation_impl.h
+++ src/sst/core/simulation_impl.h
@@ -412,6 +412,11 @@ public:
      */
     void signalShutdown(bool abnormal);
 
+    /** Interactive Shutdown
+     * Called when an interactive console needs to terminate SST
+     */
+    void interactiveShutdown();
+
     /** Normal Shutdown
      */
     void endSimulation();
