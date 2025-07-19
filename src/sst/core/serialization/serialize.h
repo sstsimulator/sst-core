a3d2c89125f105fa56183b35bc35d76c30a338df
leekillough <15950023+leekillough@users.noreply.github.com>
2025-07-19 15:31:58 -0500

    Serialize aggregate class types

--- src/sst/core/serialization/serialize.h
+++ src/sst/core/serialization/serialize.h
@@ -448,6 +448,7 @@ sst_ser_or_helper(Args... args)
 // warnings.
 #define SST_INCLUDING_SERIALIZE_H
 #include "sst/core/serialization/impl/serialize_adapter.h"
+#include "sst/core/serialization/impl/serialize_aggregate.h"
 #include "sst/core/serialization/impl/serialize_array.h"
 #include "sst/core/serialization/impl/serialize_atomic.h"
 #include "sst/core/serialization/impl/serialize_bitset.h"
