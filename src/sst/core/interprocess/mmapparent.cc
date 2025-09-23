256e5d04f955300e3f353cdb69c4cf8f4fd42455
Patrick Lavin <plavin997@gmail.com>
2025-09-23 10:22:38 -0400

    Expose MPI_Comm_spawn_multiple to elements (#1414)
    * change ariel api spawn api

--- src/sst/core/interprocess/mmapparent.cc
+++ src/sst/core/interprocess/mmapparent.cc
@@ -16,6 +16,75 @@
 #include "sst/core/interprocess/mmapparent.h"
 
 #include "sst/core/interprocess/tunneldef.h"
+#include "sst/core/sst_mpi.h"
 
 using testtunnel = SST::Core::Interprocess::TunnelDef<int, int>;
 template class SST::Core::Interprocess::MMAPParent<testtunnel>;
+
+namespace SST::Core::Interprocess {
+
+int
+SST_MPI_Comm_spawn_multiple(int count, char* array_of_commands[], char** array_of_argv[], const int array_of_maxprocs[],
+    const char* array_of_env[])
+{
+
+#ifdef SST_CONFIG_HAVE_MPI
+    // Count the maximum number of ranks that may be launched
+    int ranks = 0;
+    for ( int i = 0; i < count; i++ ) {
+        ranks += array_of_maxprocs[i];
+    }
+
+    int* array_of_errcodes = (int*)malloc(sizeof(int) * ranks);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int  name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Passing environment variables to child processes is implementation-specific.
+    // Documentation for MPI_Info options:
+    // https://docs.open-mpi.org/en/main/man-openmpi/man3/MPI_Comm_spawn_multiple.3.html#info-arguments
+    MPI_Info* array_of_info = (MPI_Info*)malloc(sizeof(MPI_Info) * count);
+    for ( int i = 0; i < count; i++ ) {
+        MPI_Info_create(&array_of_info[i]);
+        MPI_Info_set(array_of_info[i], "host", processor_name);
+
+        // Do not set the child's environment if array_of_env[i] is empty, which seems to crash.
+        if ( strcmp("", array_of_env[i]) ) {
+            MPI_Info_set(array_of_info[i], "env", array_of_env[i]);
+        }
+    }
+
+    MPI_Comm intercomm;
+
+    int result = MPI_Comm_spawn_multiple(count, array_of_commands, array_of_argv, array_of_maxprocs, array_of_info, 0,
+        MPI_COMM_SELF, &intercomm, array_of_errcodes);
+
+    // Handle errors here as doing so requires MPI macros and functions
+    int errors = 0;
+    if ( result != MPI_SUCCESS ) {
+        char error_string[MPI_MAX_ERROR_STRING];
+        int  length_of_error_string;
+        MPI_Error_string(result, error_string, &length_of_error_string);
+        fprintf(stderr, "Error in MPI_Comm_spawn_multiple: %s\n", error_string);
+        errors++;
+    }
+    else {
+        for ( int i = 0; i < ranks; i++ ) {
+            if ( array_of_errcodes[i] != MPI_SUCCESS ) {
+                fprintf(stderr, "Error in MPI_Comm_spawn_multiple: process %d failed to start\n", i);
+                errors++;
+            }
+        }
+    }
+
+    free(array_of_info);
+    free(array_of_errcodes);
+
+    return errors;
+#else
+    fprintf(stderr, "SST_MPI_Comm_spawn_multiple called but SST-Core was compiled without MPI\n");
+    return 1;
+#endif
+}
+} // namespace SST::Core::Interprocess
