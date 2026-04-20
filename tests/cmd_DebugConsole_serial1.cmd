dd0440f354d6cc6fc22bc945692cce63cd1c81f8
Shannon Kuntz <skuntz@tactcomplabs.com>
2026-04-20 15:07:47 -0500

    Add debug console trigger/action tests for serial and parallel execution

---
+++ tests/cmd_DebugConsole_serial1.cmd
@@ -0,0 +1,45 @@
+confirm false
+# 0 watch
+cd component0
+watch message_count_ changed
+run
+wl
+unwatch 0
+cd ..
+# 1 trace printStatus
+cd component2
+trace message_count_ changed : 16 0 : message_count_ : printStatus
+setHandler 1 ae
+run 1ps
+wl
+unwatch 1
+# 2 trace printTrace
+cd ..
+cd component5
+trace message_count_ changed : 16 4 : message_count_ : printTrace
+setHandler 2 ae
+run 1000ps
+wl
+unwatch 2
+# 3 trace set
+cd ..
+cd component6
+trace message_count_ changed : 16 2 : message_count_ : set my_id_ 50
+setHandler 3 ae
+run 1000ps
+wl
+#print my_id_
+unwatch 3
+# 4 trace interactive
+cd ..
+cd component10
+trace message_count_ changed : 16 2 : message_count_ : interactive
+setHandler 4 ae
+run
+wl
+unwatch 4
+# 5 trace shutdown
+cd ..
+cd component12
+trace message_count_ changed : 16 2 : message_count_ : shutdown
+run
