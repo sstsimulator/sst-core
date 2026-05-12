e96633cc855e8ab6da8cc5a96c3fb370feedeb85
Justin M. LaPre <110123055+jmlapre@users.noreply.github.com>
2026-05-12 14:03:14 -0400

    Fixes for ThreadSanitizer (#1617)
    * Add lock_guard to Barrier::resize()
    
    * Move ic_barrier_ resize to setupSyncObjects()

--- src/sst/core/threadsafe.h
+++ src/sst/core/threadsafe.h
@@ -51,6 +51,7 @@ class CACHE_ALIGNED_T Barrier
     size_t              origCount;
     std::atomic<bool>   enabled;
     std::atomic<size_t> count, generation;
+    std::mutex          barrierMutex;
 
 public:
     Barrier(size_t count) :
@@ -71,6 +72,7 @@ public:
     /** ONLY call this while nobody is in wait() */
     void resize(size_t newCount)
     {
+        std::lock_guard<std::mutex> lk(barrierMutex);
         count = origCount = newCount;
         generation.store(0);
         enabled.store(true);
